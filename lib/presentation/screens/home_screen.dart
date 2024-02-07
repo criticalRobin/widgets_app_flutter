15ce32e17907abe9abe4a5f935dd3917dff802f7
matite <matiastite1@gmail.com>
2024-02-07 09:43:11 -0500

    Created _HomeView and _CustomListTile widgets to use in the HomScreen widget

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,52 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Fultter + Material 3"),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        menuItem.icon,
+        color: color.primary,
+      ),
+      title: Text(menuItem.title),
+      trailing: Icon(
+        Icons.arrow_forward_ios_outlined,
+        color: color.primary,
+      ),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        //ToDo: Navigate through the screens
+      },
     );
   }
 }
