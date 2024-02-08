62be3abc1335816e216f0e0351128d4db43c3ae7
matite <matiastite1@gmail.com>
2024-02-08 10:03:01 -0500

    Created _CardView widget, and example of how to use SingleChildScrollView widget

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {"elevation": 0.0, "label": "Elevation 0"},
+  {"elevation": 1.0, "label": "Elevation 1"},
+  {"elevation": 2.0, "label": "Elevation 2"},
+  {"elevation": 3.0, "label": "Elevation 3"},
+  {"elevation": 4.0, "label": "Elevation 4"},
+  {"elevation": 5.0, "label": "Elevation 5"},
+];
+
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
 
@@ -9,7 +18,55 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Cards Screen"),
       ),
-      body: const Placeholder(),
+      body: const _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map((card) =>
+              _CardType1(label: card["label"], elevation: card["elevation"])),
+          ...cards.map((card) =>
+              _CardType1(label: card["label"], elevation: card["elevation"]))
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(label),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
