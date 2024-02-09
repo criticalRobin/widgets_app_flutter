97f38c77816d1601c2cde4b08248b130bfb73aec
matite <matiastite1@gmail.com>
2024-02-09 09:00:19 -0500

    Screens created and export file updated

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  static const name = "app_tutorial_screen";
+
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  static const name = "ui_screen";
+
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
