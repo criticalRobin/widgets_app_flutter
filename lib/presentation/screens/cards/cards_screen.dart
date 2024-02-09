9fc5e6536b16fe76b855085f84920afebfc6be88
matite <matiastite1@gmail.com>
2024-02-09 08:16:26 -0500

    Example of card tupe 2 using borders

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -33,8 +33,8 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map((card) =>
               _CardType1(label: card["label"], elevation: card["elevation"])),
-          ...cards.map((card) =>
-              _CardType1(label: card["label"], elevation: card["elevation"]))
+          ...cards.map(
+              (e) => _CardType2(label: e["label"], elevation: e["elevation"]))
         ],
       ),
     );
@@ -63,6 +63,41 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.centerLeft,
               child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("$label - outlined card"),
             )
           ],
         ),
