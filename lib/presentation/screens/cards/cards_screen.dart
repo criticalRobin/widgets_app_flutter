934e2fbb62543cf4dab8b74726393a83b2671949
matite <matiastite1@gmail.com>
2024-02-09 08:42:05 -0500

    Example of how to create cards with images

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -34,7 +34,14 @@ class _CardsView extends StatelessWidget {
           ...cards.map((card) =>
               _CardType1(label: card["label"], elevation: card["elevation"])),
           ...cards.map(
-              (e) => _CardType2(label: e["label"], elevation: e["elevation"]))
+              (e) => _CardType2(label: e["label"], elevation: e["elevation"])),
+          ...cards.map(
+              (e) => _CardType3(label: e["label"], elevation: e["elevation"])),
+          ...cards.map(
+              (e) => _CardType4(label: e["label"], elevation: e["elevation"])),
+          const SizedBox(
+            height: 50,
+          )
         ],
       ),
     );
@@ -101,6 +108,75 @@ class _CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("$label - Filled"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            "https://picsum.photos/id/${elevation.toInt()}/600/350",
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20)),
+                ),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined)),
+              ))
+        ],
       ),
     );
   }
