72218a1028210b5e877565b07d8dcf92d1c9d8fe
matite <matiastite1@gmail.com>
2024-02-15 11:02:59 -0500

    Fixed HomeScreen import

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home_screen.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: "/",
