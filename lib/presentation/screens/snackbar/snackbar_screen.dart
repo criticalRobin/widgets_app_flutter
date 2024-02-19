d024dc3a9fa508942848b62512b9a5f04de0f651
matite <matiastite1@gmail.com>
2024-02-19 11:35:49 -0500

    Example of how to use dialog widgets

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = "snackbar_screen";
@@ -17,11 +18,46 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text("Estas seguro?"),
+              content: const Text("Esto es una prueba"),
+              actions: [
+                TextButton(
+                    onPressed: () => context.pop(),
+                    child: const Text("Cancelar")),
+                FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text("Aceptar")),
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Snackbar Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(context: context, children: [
+                    const Text("Todas las licencias usadas en esta app son")
+                  ]);
+                },
+                child: const Text("Licencias Usadas")),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text("Mostrar Diálogo")),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showSnackBar(context),
