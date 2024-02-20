afadd8430e01c896d77064a45e4af44ccf1445fa
matite <matiastite1@gmail.com>
2024-02-20 11:30:28 -0500

    ExpansionTile and CheckBoxTile examples

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -28,6 +28,10 @@ enum Transportation { car, bus, train, plane }
 class _UiControlsViewState extends State<_UiControlsView> {
   bool isDeveloperMode = true;
   Transportation selectedTransportation = Transportation.car;
+  bool wantToReceiveNotifications = false;
+  bool wantToReceiveEmails = false;
+  bool wantToReceiveCalls = false;
+  bool wantToReceiveMessages = false;
 
   @override
   Widget build(BuildContext context) {
@@ -40,38 +44,78 @@ class _UiControlsViewState extends State<_UiControlsView> {
             onChanged: (value) {
               setState(() => isDeveloperMode = !isDeveloperMode);
             }),
-        RadioListTile(
-            title: const Text("Carro"),
-            subtitle: const Text("Transporte personal"),
-            value: Transportation.car,
-            groupValue: selectedTransportation,
+        ExpansionTile(
+          title: const Text("Trasnporte"),
+          subtitle: Text('$selectedTransportation'),
+          children: [
+            RadioListTile(
+                title: const Text("Carro"),
+                subtitle: const Text("Transporte personal"),
+                value: Transportation.car,
+                groupValue: selectedTransportation,
+                onChanged: (value) => setState(() {
+                      selectedTransportation = Transportation.car;
+                    })),
+            RadioListTile(
+                title: const Text("Bus"),
+                subtitle: const Text("Transporte público"),
+                value: Transportation.bus,
+                groupValue: selectedTransportation,
+                onChanged: (value) => setState(() {
+                      selectedTransportation = Transportation.bus;
+                    })),
+            RadioListTile(
+                title: const Text("Tren"),
+                subtitle: const Text("Transporte a vapor"),
+                value: Transportation.train,
+                groupValue: selectedTransportation,
+                onChanged: (value) => setState(() {
+                      selectedTransportation = Transportation.train;
+                    })),
+            RadioListTile(
+                title: const Text("Avión"),
+                subtitle: const Text("Transporte aéreo"),
+                value: Transportation.plane,
+                groupValue: selectedTransportation,
+                onChanged: (value) => setState(() {
+                      selectedTransportation = Transportation.plane;
+                    })),
+          ],
+        ),
+        CheckboxListTile(
+            title: const Text("Notificaciones"),
+            subtitle: const Text("Recibir notificaciones"),
+            value: wantToReceiveNotifications,
             onChanged: (value) => setState(() {
-                  selectedTransportation = Transportation.car;
+                  wantToReceiveNotifications = !wantToReceiveNotifications;
                 })),
-        RadioListTile(
-            title: const Text("Bus"),
-            subtitle: const Text("Transporte público"),
-            value: Transportation.bus,
-            groupValue: selectedTransportation,
-            onChanged: (value) => setState(() {
-                  selectedTransportation = Transportation.bus;
-                })),
-        RadioListTile(
-            title: const Text("Tren"),
-            subtitle: const Text("Transporte a vapor"),
-            value: Transportation.train,
-            groupValue: selectedTransportation,
-            onChanged: (value) => setState(() {
-                  selectedTransportation = Transportation.train;
-                })),
-        RadioListTile(
-            title: const Text("Avión"),
-            subtitle: const Text("Transporte aéreo"),
-            value: Transportation.plane,
-            groupValue: selectedTransportation,
-            onChanged: (value) => setState(() {
-                  selectedTransportation = Transportation.plane;
-                })),
+        CheckboxListTile(
+            title: const Text("Llamadas"),
+            subtitle: const Text("Recibir llamadas"),
+            value: wantToReceiveCalls,
+            onChanged: (value) {
+              setState(() {
+                wantToReceiveCalls = !wantToReceiveCalls;
+              });
+            }),
+        CheckboxListTile(
+            title: const Text("Emails"),
+            subtitle: const Text("Recibir emails"),
+            value: wantToReceiveEmails,
+            onChanged: (value) {
+              setState(() {
+                wantToReceiveEmails = !wantToReceiveEmails;
+              });
+            }),
+        CheckboxListTile(
+            title: const Text("Mensajes"),
+            subtitle: const Text("Recibir mensajes"),
+            value: wantToReceiveMessages,
+            onChanged: (value) {
+              setState(() {
+                wantToReceiveMessages = !wantToReceiveMessages;
+              });
+            }),
       ],
     );
   }
