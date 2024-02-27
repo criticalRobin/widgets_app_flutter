ea799fc1d3c6ff9d1bf83166f3b0e1b462bba229
matite <matiastite1@gmail.com>
2024-02-27 07:57:25 -0500

    Created provider to manage light ot dark theme

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
@@ -8,10 +9,21 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickValue = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Counter Riverpod"),
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode
+                ? Icons.dark_mode_outlined
+                : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).state = !isDarkMode;
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text(
@@ -20,7 +32,9 @@ class CounterScreen extends ConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
         child: const Icon(Icons.add_rounded),
       ),
     );
